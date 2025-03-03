6ef856eb2e8c714d17ab6a0d6f9eb7fd6720c6ca
amos-uxf24 <amos.linne@chasacademy.se>
2025-03-03 15:25:32 +0100

    koopa footer komponent

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import AboutPage from "./pages/AboutPage";
+import Footer from './components/Footer';
 
 function App() {
     return (
@@ -12,11 +13,12 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<ProductsPage />} />
-                <Route path="/product/:id" element={<ProductPage />} /> {/* ✅ Correct path */}
+                <Route path="/product/:id" element={<ProductPage />} />
                 <Route path="/cart" element={<CartPage />} />
                 <Route path="/order-confirmation" element={<OrderPage />} />
                 <Route path="/about" element={<AboutPage />} />
             </Routes>
+            <Footer /> {/* Lägg till Footer här */}
         </>
     );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,6 @@
-Footer.jsx
-
-
-// src/components/Footer.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Footer.css';
+import '../styles.css';
 
 function Footer() {
   return (
