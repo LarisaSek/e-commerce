141a26db7e36058de3a391dd21cc8ffdf69e776e
LarisaSek <larisa.sekiro@chasacademy.se>
2025-03-03 14:00:34 +0100

    Prittier formatering

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,30 @@
-//Larisa 
-import { useCart } from '../context/CartContext';
+import { useCart } from "../context/CartContext";
 
 const Cart = () => {
-    const { cart, dispatch } = useCart();
-    return (
-        <div>
-            <h2>Shopping Cart</h2>
-            {cart.length === 0 ? <p>Your cart is empty</p> : cart.map(item => (
-                <div key={item.id}>
-                    <h3>{item.title}</h3>
-                    <p>Price: ${item.price}</p>
-                    <p>Quantity: {item.quantity || 1}</p>
-                    <button onClick={() => dispatch({ type: 'REMOVE_FROM_CART', payload: item.id })}>Remove</button>
-                </div>
-            ))}
-        </div>
-    );
+  const { cart, dispatch } = useCart();
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      {cart.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        cart.map((item) => (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <p>Price: ${item.price}</p>
+            <p>Quantity: {item.quantity || 1}</p>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_FROM_CART", payload: item.id })
+              }
+            >
+              Remove
+            </button>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Cart;
